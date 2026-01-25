fd2562831262995de23977f59352fd42bdd09bb8
Carlos-iso <carlosbalak2002@gmail.com>
2026-01-25 10:25:47 -0300

    Saindo

--- src/models/user.js
+++ src/models/user.js
@@ -1,8 +1,6 @@
 'use strict';
-
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
 const schema = new Schema({
     name: {
         type: String,
@@ -31,5 +29,4 @@ const schema = new Schema({
         default: Date.now
     }
 });
-
 module.exports = mongoose.model('User', schema);
